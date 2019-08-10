2cf4321f8fdb30b22489f866740f8b663e1ac9d8
Rodrigo Alcala <alcala@myego2go.de>
2019-08-11 00:45:51 +0200

    feat: first functional commit

--- controllers.js
+++ controllers.js
@@ -1,8 +1,42 @@
+const forrites = {
+  M2tLnmk: 'ariel', 
+  Xmwdme6: 'evelin', 
+  voHVeGX: 'federico', 
+  jTZX5Hh: 'ignacio', 
+  kEw1P1x: 'marco', 
+  fpFr67v: 'rodrigo', 
+  DvQ3TW1: 'santiago', 
+  WMNhGxh: 'solana', 
+  eBHBQEh: 'yamila'
+};
+
+const asistencia = {
+  ariel: false,
+  evelin: false,
+  federico: false,
+  ignacio: false,
+  marco: false,
+  rodrigo: false,
+  santiago: false,
+  solana: false,
+  yamila: false
+}
+
 exports.handleVote = (ctx, db) => {
   try {
+    const votante = forrites[ctx.params.votante];
     const { vote } = ctx.request.body;
+    if (asistencia[votante]) {
+      ctx.status = 407;
+      ctx.body = JSON.stringify({
+        error: 'Un vote por forrite, campeon.',
+      });
+      console.log(`${votante} quizo votar de nuevo, esta vez por ${vote}`)
+      return;
+    }
     db[vote] ++;
-    console.log(`${ctx.params.votante} acaba de votar por ${vote}`);
+    asistencia[votante] = true;
+    console.log(`${votante} acaba de votar por ${vote}`);
     ctx.status = 200;
   } catch (err) {
     console.log(err); // eslint-disable-line
@@ -14,7 +48,10 @@ exports.showResults = (ctx, db) => {
   try {
     console.log('Pidiendo los resultados');
     ctx.status = 200;
-    ctx.body = JSON.stringify(db);
+    ctx.body = {
+      resultados: db,
+      asistencia
+    };
   } catch (err) {
     console.log(err); // eslint-disable-line
     ctx.status = 500;
