1f0696f63ed71f46c6d4f2023faa02c6be9038fb
Rodrigo Alcala <alcala@myego2go.de>
2019-08-11 13:07:51 +0200

    feat: add interval to keep dyno alive

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ require('dotenv').config({ path: '.env' });
 const Koa = require('koa');
 const app = new Koa();
 
+const fetch = require('node-fetch');
 const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
 const router = require('./router');
 
+setInterval(() => {
+  fetch('http://losforritos.herokuapp.com/resultados')
+    .then(() => console.log('KeepAlive runned succesfully!'))
+    .error(err => console.log(err))
+}, 20 * 60 * 1000);
 
 app.use(bodyParser());
 app.use(cors());
